a21c733f446ca49f15e23cd8545155bb5f882963
Caleb Leung <caleb.leungg@gmail.com>
2020-01-26 18:10:38 +1000

    adjustings to dynamic routes, candidates now correctly allocate to jobs, jobs correctly link to different managing pages

--- rails-version/app/controllers/job_controller.rb
+++ rails-version/app/controllers/job_controller.rb
@@ -1,23 +1,41 @@
 class JobController < ApplicationController
     skip_before_action :verify_authenticity_token
     before_action :setup_jobs
+    attr_reader :candidate
 
     def index
         # render layout: "job_manager"
     end
 
     def job_manager
-        render layout: "job_manager"
-    end
+        @job = @jobs.find(params[:job_id])
+        @candidate_pool = @candidates.where(job_id: params[:job_id])
 
-    def candidate
-        @candidates.each do | candidate |
-            if candidate[:id] == params[:id].to_i
-                @candidate = candidate
-            else
-                @error = "Error: non-existent candidate"
+        @columns = { "applied" => 0, "contacted" => 1, "screened" => 2, "shortlisted" => 3, "interview" => 4, "offer" => 5, "accepted" => 6, "disqualified" => 7}
+        @repeats = Hash.new(0)
+
+        if @candidate_pool != []
+            @candidate_pool.each do | candidate |
+                if candidate[:id] == params[:id].to_i
+                    @candidate = candidate
+                else
+                    @error = "Error: non-existent candidate"
+                end
+
+                @repeats[candidate[:status]] += 1
+            end
+        
+            @row_count = @repeats.values.max
+            @row_data = []
+
+            until @row_count == 0 
+                @row_data.push(["", "", "", "", "", "", "", ""])
+                @row_count -= 1
             end
         end
+
+        @candidate = @candidates.where(id: params[:candidate_id]).first
+
         render layout: "job_manager"
     end
 
--- rails-version/config/routes.rb
+++ rails-version/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/", to: "job#index", as: "root"
-  get "/candidate/:id", to: "job#candidate", as: "candidate"
-  get "/job-manager", to: "job#job_manager", as: "manager"
+  get "/job-manager/:job_id", to: "job#job_manager", as: "manager"
+  get "/job-manager/:job_id/candidate/:candidate_id", to: "job#job_manager", as: "candidate"
 
 end
